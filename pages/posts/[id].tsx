6f2b2184da72715f608eac4bd3518ab9e4a025e6
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-27 09:39:21 +0900

    Blog 글 클릭 시 상세페이지로 이동하여 .md 파일의 내용을 출력해주는 기능 추가

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,7 +1,49 @@
+import { GetStaticPaths, GetStaticProps } from "next";
 import React from "react";
+import { getPostData, gettAllPostIds } from "../../lib/post";
+import Head from "next/head";
+import homeStyled from "../../styles/home.module.css";
 
-const Post = () => {
-  return <div>Post</div>;
+const Post = ({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1>{postData.title}</h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </div>
+  );
 };
 
 export default Post;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = gettAllPostIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params?.id as string);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+};
